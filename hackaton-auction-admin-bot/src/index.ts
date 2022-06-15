9255c77e9793812bcbb68d44c023155580fd0dae
dmitriyb <dmitriyb@wix.com>
2022-06-15 16:09:33 +0300

    chore: bot launch message

--- hackaton-auction-admin-bot/src/index.ts
+++ hackaton-auction-admin-bot/src/index.ts
@@ -1,20 +1,34 @@
-import { Telegraf } from 'telegraf'
-import 'dotenv/config'
+import {Telegraf} from 'telegraf';
+import 'dotenv/config';
 
 if (!process.env.BOT_TOKEN) {
-  throw new Error('no BOT_TOKEN provided')
+  throw new Error('no BOT_TOKEN provided');
 }
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('test', (ctx) => {
-  ctx.reply('Hello!')
-  ctx.reply(`I'm 'hackaton-auction-admin-bot'`)
-  ctx.reply(`You are @${ctx.message.from.username}. Your id – ${ctx.message.from.id}`)
-})
+bot.command('test', ctx => {
+  ctx.reply('Hello!');
+  ctx.reply(`I'm 'hackaton-auction-admin-bot'`);
+  ctx.reply(
+    `You are @${ctx.message.from.username}. Your id – ${ctx.message.from.id}`
+  );
+});
 
-bot.launch()
+bot
+  .launch()
+  .catch(reason => {
+    console.error('Failed to run');
+    bot.stop(reason);
+  })
+  .then(() => {
+    console.log(
+      'the bot is up and running',
+      bot.botInfo,
+      `https://t.me/${bot.botInfo?.username}`
+    );
+  });
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
