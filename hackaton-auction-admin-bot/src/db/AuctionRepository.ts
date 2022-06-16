4ef4961a23c53b57ba241a8fd5776ff1b7e13c4d
Kyrylo Hrecheniuk <kyryloh@wix.com>
2022-06-16 09:57:53 +0200

    yarn lint:fix

--- hackaton-auction-admin-bot/src/db/AuctionRepository.ts
+++ hackaton-auction-admin-bot/src/db/AuctionRepository.ts
@@ -1,29 +1,29 @@
-import { randomUUID } from 'crypto'
-import type { Db } from 'mongodb'
+import {randomUUID} from 'crypto';
+import type {Db} from 'mongodb';
 
 type Auction = {
-    id: string
-    title: string
-}
+  id: string;
+  title: string;
+};
 
 export class AuctionRepository {
-    private readonly AUCTIONS_COLLECTION = 'auctions'
+  private readonly AUCTIONS_COLLECTION = 'auctions';
 
-    constructor(protected readonly db: Db) {}
+  constructor(protected readonly db: Db) {}
 
-    async create(auction: Omit<Auction, 'id'>): Promise<void> {
-        const id = randomUUID()
-        await this.db.collection(this.AUCTIONS_COLLECTION).insertOne({
-            id,
-            title: auction.title,
-        })
-    }
+  async create(auction: Omit<Auction, 'id'>): Promise<void> {
+    const id = randomUUID();
+    await this.db.collection(this.AUCTIONS_COLLECTION).insertOne({
+      id,
+      title: auction.title,
+    });
+  }
 
-    findAll(): Promise<Auction[]> {
-        let cursor = this.db
-            .collection(this.AUCTIONS_COLLECTION)
-            .find<Auction>({})
+  findAll(): Promise<Auction[]> {
+    const cursor = this.db
+      .collection(this.AUCTIONS_COLLECTION)
+      .find<Auction>({});
 
-        return cursor.toArray()
-    }
+    return cursor.toArray();
+  }
 }
--- hackaton-auction-bot/src/index.ts
+++ hackaton-auction-bot/src/index.ts
@@ -1,29 +1,29 @@
 import {Telegraf, Context} from 'telegraf';
-import 'dotenv/config'
-import {Db, MongoClient} from "mongodb";
+import 'dotenv/config';
+import {Db, MongoClient} from 'mongodb';
 import {ExampleShared} from 'hackaton-auction-common';
 
 const example = new ExampleShared();
 example.test();
 
-const url = "mongodb://localhost:27017/"; // TODO: move it to `.env`
-const DB_NAME = "auction_bot" // TODO: move it to `.env`
+const url = 'mongodb://localhost:27017/'; // TODO: move it to `.env`
+const DB_NAME = 'auction_bot'; // TODO: move it to `.env`
 
 if (!process.env.BOT_TOKEN) {
-  throw new Error('no BOT_TOKEN provided')
+  throw new Error('no BOT_TOKEN provided');
 }
 
 interface AppContext extends Context {
-  db: Db
+  db: Db;
 }
 
-const bot = new Telegraf<AppContext>(process.env.BOT_TOKEN)
+const bot = new Telegraf<AppContext>(process.env.BOT_TOKEN);
 
 bot.use(async (ctx, next) => {
-  const connection = await MongoClient.connect(url)
-  ctx.db = connection.db(DB_NAME)
-  return next()
-})
+  const connection = await MongoClient.connect(url);
+  ctx.db = connection.db(DB_NAME);
+  return next();
+});
 
 bot.command('test', ctx => {
   ctx.reply('Hello!');
