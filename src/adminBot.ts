0f8e2c991477b98fa187cd9ec705426bdbf1e417
dmitriyb <dmitriyb@wix.com>
2022-06-17 22:46:38 +0300

    persistent session

--- src/adminBot.ts
+++ src/adminBot.ts
@@ -1,4 +1,4 @@
-import {Telegraf, Scenes, session, Context, Middleware} from 'telegraf';
+import {Telegraf, Scenes, Middleware} from 'telegraf';
 import 'dotenv/config';
 import {MongoClient} from 'mongodb';
 import {AppContext, VolunteerSessionData} from './types';
@@ -20,6 +20,8 @@ import {randomUUID} from "crypto";
 import {PhotoSize} from "typegram";
 import {VolunteerService} from "./services/VolunteerService";
 import {BidService} from "./services/BidService";
+import {getDb} from './db/connection';
+import {session} from 'telegraf-session-mongodb';
 
 const CREATE_AUCTION_SCENE = 'CREATE_AUCTION_SCENE2';
 
@@ -66,128 +68,131 @@ const {BOT_ADMIN_TOKEN, DB_NAME, DB_URL} = process.env;
 if (!BOT_ADMIN_TOKEN) {
   throw new Error('no BOT_ADMIN_TOKEN provided');
 }
-if (!DB_NAME) {
-  throw new Error('no DB_NAME provided');
-}
-if (!DB_URL) {
-  throw new Error('no DB_URL provided');
-}
 
 export const adminBot = new Telegraf<AppContext>(BOT_ADMIN_TOKEN);
 
 adminBot.use(Telegraf.log());
-adminBot.use(session());
+// adminBot.use(session());
+getDb().then(db => {
+  adminBot.use(session(db, {collectionName: 'adminSessions'}));
 
-adminBot.use(async (ctx, next) => {
-  const connection = await MongoClient.connect(DB_URL);
-  ctx.db = connection.db(DB_NAME);
-  ctx.session ??= {};
-  return next();
-});
+  adminBot.use(async (ctx, next) => {
+    // const connection = await MongoClient.connect(DB_URL);
+    ctx.db = db;
+    ctx.session ??= {};
+    return next();
+  });
 
-adminBot.use(async (ctx, next) => {
-  const auctionRepo = new AuctionRepository(ctx.db);
-  const volunteer = ctx.session.volunteer;
-  console.log('volunteer', volunteer);
-  if (volunteer) {
-    ctx.session.activeAuction =
-      (await auctionRepo.findActive(volunteer.id!)) || undefined;
-    console.log('ctx.session.activeAuction', ctx.session.activeAuction);
-  }
-  return next();
-});
+  adminBot.use(async (ctx, next) => {
+    const auctionRepo = new AuctionRepository(ctx.db);
+    const volunteer = ctx.session.volunteer;
+    console.log('volunteer', volunteer);
+    if (volunteer) {
+      ctx.session.activeAuction =
+        (await auctionRepo.findActive(volunteer.id!)) || undefined;
+      console.log('ctx.session.activeAuction', ctx.session.activeAuction);
+    }
+    return next();
+  });
 
-const createAuctionScene = new Scenes.BaseScene<AuctionSceneContext>(
-  CREATE_AUCTION_SCENE
-);
-createAuctionScene.enter(async ctx => {
-  let currentStep = 0;
-  const volunteer = ctx.session.volunteer;
-  if (!volunteer) {
-    await ctx.reply(
-      'Щось мабуть пішло не так, дуже шкода, бумласка, спробуйте почати заново /start'
-    );
-    createAuctionScene.leave();
-    return;
-  }
-  if (ctx.session.activeAuction) {
-    if (ctx.session.activeAuction.status === 'closed') {
-      delete ctx.session.activeAuction;
-    } else {
+  const createAuctionScene = new Scenes.BaseScene<AuctionSceneContext>(
+    CREATE_AUCTION_SCENE
+  );
+  createAuctionScene.enter(async ctx => {
+    let currentStep = 0;
+    const volunteer = ctx.session.volunteer;
+    if (!volunteer) {
       await ctx.reply(
-        'В вас вже є активний аукціон, нажаль ми наразі тільки один. Ви зможете створити новий, коли закінчите цей.'
+        'Щось мабуть пішло не так, дуже шкода, бумласка, спробуйте почати заново /start'
       );
+      createAuctionScene.leave();
       return;
     }
-  }
-  const next = async () => {
-    currentStep++;
-    if (currentStep < auctionFields.length) {
-      await ctx.reply(auctionFields[currentStep].prompt);
-    } else {
-      const auctionsService = new AuctionService(ctx.db)
-      const auction: Auction = {
-        ...ctx.session.auctionData,
+    if (ctx.session.activeAuction) {
+      if (ctx.session.activeAuction.status === 'closed') {
+        delete ctx.session.activeAuction;
+      } else {
+        await ctx.reply(
+          'В вас вже є активний аукціон, нажаль ми наразі тільки один. Ви зможете створити новий, коли закінчите цей.'
+        );
+        return;
+      }
+    }
+    const next = async () => {
+      currentStep++;
+      if (currentStep < auctionFields.length) {
+        await ctx.reply(auctionFields[currentStep].prompt);
+      } else {
+        const auctionsService = new AuctionService(ctx.db)
+        const auction : Auction ={
+          ...auctionData,
         id: randomUUID(),
-        volunteerId: ctx.session.volunteer?.id!,
+          volunteerId: ctx.session.volunteer?.id!,
         status: 'opened',
       }
       await auctionsService.create(auction)
-      ctx.session.activeAuction = auction;
-      await ctx.reply(`Вітаю!
+        ctx.session.activeAuction = auction;
+        await ctx.reply(`Вітаю!
 Аукціон створено.
 Наступний крок — залучити якнайбільше учасників.
 Посилання на аукціон: https://t.me/${process.env.AUCTION_BOT_NAME}?start=${auction.id}`);
-      createAuctionScene.leave();
-    }
-  };
-  await ctx.reply(auctionFields[currentStep].prompt);
-  ctx.session.auctionData = {
-    title: '',
-    description: '',
-    photos: [],
-    startBet: 0,
-    volunteerId: volunteer.id!,
-    status: 'opened',
-  };
-  createAuctionScene.on('text', async ctx => {
-    const currentField = auctionFields[currentStep].id;
-    switch (currentField) {
-      case 'title': {
-        ctx.session.auctionData.title = ctx.message.text;
-        await next();
-        break;
+        createAuctionScene.leave();
       }
-      case 'description': {
-        ctx.session.auctionData.description = ctx.message.text;
-        await next();
-        break;
-      }
-      case 'startBet': {
-        ctx.session.auctionData.startBet = Number(ctx.message.text);
-        await next();
-        break;
-      }
-      default:
-        break;
-    }
-  });
-  createAuctionScene.on('photo', async ctx => {
-    const currentField = auctionFields[currentStep].id;
-    switch (currentField) {
-      case 'photos': {
-        ctx.session.auctionData.photos = ctx.message.photo;
-        await next();
-        break;
-      }
-      default:
-        break;
-    }
-  });
-});
+    };
+    await ctx.reply(auctionFields[currentStep].prompt);
+    const auctionData: NewAuction = {
+      title: '',
+      description: '',
+      photos: [],
+      startBet: 0,
+      volunteerId: volunteer.id!,
+      status: 'opened',
 
-adminBot.start(async ctx => {
-  const volunteerService = new VolunteerService(ctx.db)
+    };
+    createAuctionScene.on('text', async ctx => {
+      if (currentStep >= auctionFields.length) {
+        return;
+      }
+      const currentField = auctionFields[currentStep].id;
+      switch (currentField) {
+        case 'title': {
+          auctionData.title = ctx.message.text;
+          await next();
+          break;
+        }
+        case 'description': {
+          auctionData.description = ctx.message.text;
+          await next();
+          break;
+        }
+        case 'startBet': {
+          auctionData.startBet = Number(ctx.message.text);
+          await next();
+          break;
+        }
+        default:
+          break;
+      }
+    });
+    createAuctionScene.on('photo', async ctx => {
+      if (currentStep >= auctionFields.length) {
+        return;
+      }
+      const currentField = auctionFields[currentStep].id;
+      switch (currentField) {
+        case 'photos': {
+          auctionData.photos = ctx.message.photo;
+          await next();
+          break;
+        }
+        default:
+          break;
+      }
+    });
+  });
+
+  adminBot.start(async ctx => {
+    const volunteerService = new VolunteerService(ctx.db)
 
   const { id: telegramId } = ctx.from
   const volunteer = {
@@ -214,177 +219,178 @@ adminBot.start(async ctx => {
 
 Не гаємо часу і розпочинаємо! 
 Успіхів.`);
-});
 
-// type AuctionStageContext = WrapSceneContext<AuctionSceneContext>;
-// TODO: do it more properly with the wrapper
-const stage = new Scenes.Stage<AuctionSceneContext>([createAuctionScene]);
-adminBot.use(stage.middleware() as unknown as Middleware<AppContext>);
+  });
 
-adminBot.command('create', ctx => {
-  // @ts-ignore
-  ctx.scene.enter(CREATE_AUCTION_SCENE);
-});
+  // type AuctionStageContext = WrapSceneContext<AuctionSceneContext>;
+  // TODO: do it more properly with the wrapper
+  const stage = new Scenes.Stage<AuctionSceneContext>([createAuctionScene]);
+  adminBot.use(stage.middleware() as unknown as Middleware<AppContext>);
 
-adminBot.command('edit', ctx => {
-  ctx.reply('Редагувати');
-});
+  adminBot.command('create', ctx => {
+    // @ts-ignore
+    ctx.scene.enter(CREATE_AUCTION_SCENE);
+  });
 
-const SUPER_ADMINS = [
-  // devs you can add your user id here
-  45412931,
-  246078859,
-  40419532, // kyryloh@wix.com
-];
+  adminBot.command('edit', ctx => {
+    ctx.reply('Редагувати');
+  });
 
-adminBot.command('clear_mock', async ctx => {
-  const userId = ctx.message.from.id;
-  if (!SUPER_ADMINS.includes(userId)) {
-    ctx.reply('Сильно хитрий?');
-    return;
-  }
-  const auctionsService = new AuctionService(ctx.db)
-  await auctionsService.deleteAll()
+  const SUPER_ADMINS = [
+    // devs you can add your user id here
+    45412931,
+    246078859,
+    40419532, // kyryloh@wix.com
+  ];
 
-  const bidService = new BidService(ctx.db)
+  adminBot.command('clear_mock', async ctx => {
+    const userId = ctx.message.from.id;
+    if (!SUPER_ADMINS.includes(userId)) {
+      ctx.reply('Сильно хитрий?');
+      return;
+    }
+    const auctionsService = new AuctionService(ctx.db)
+    await auctionsService.deleteAll()
+    const bidService = new BidService(ctx.db)
   await bidService.deleteAll()
-});
+  });
 
-adminBot.command('fill_mock', async ctx => {
-  const auctions = arrayOf(3, () => mockAuction())
+  adminBot.command('fill_mock', async ctx => {
+    const auctions = arrayOf(3, () => mockAuction())
   const bids = arrayOf(6, () => mockBid())
 
   const auctionsService = new AuctionService(ctx.db)
-  for (const auction of auctions) {
+    for (const auction of auctions) {
     await auctionsService.create(auction);
   }
-
-  const bidService = new BidService(ctx.db)
+    const bidService = new BidService(ctx.db)
   for (const bid of bids) {
     await bidService.create(bid);
   }
-
-  await ctx.reply('Created a couple of mock auctions for you, anything else?');
-});
-
-adminBot.command('list_a', async ctx => {
-  const auctionRepo = new AuctionRepository(ctx.db);
-  const auctions = await auctionRepo.findAll();
-  await ctx.reply('Here they all are right from the DB', {
-    reply_markup: {
-      inline_keyboard: auctions.map(auction => [
-        {
-          text: auction.title,
-          callback_data: auction.id,
-        },
-      ]),
-    },
+    await ctx.reply('Created a couple of mock auctions for you, anything else?');
   });
 
-  adminBot.action(
-    auctions.map(a => a.id),
-    async ctx => {
-      const matchedAuction = auctions.find(
-        auction => auction.id === ctx.match[0]
-      );
-      await ctx.reply(JSON.stringify(matchedAuction, null, 2));
+  adminBot.command('list_a', async ctx => {
+    const auctionRepo = new AuctionRepository(ctx.db);
+    const auctions = await auctionRepo.findAll();
+    await ctx.reply('Here they all are right from the DB', {
+      reply_markup: {
+        inline_keyboard: auctions.map(auction => [
+          {
+            text: auction.title,
+            callback_data: auction.id,
+          },
+        ]),
+      },
+    });
+
+    adminBot.action(
+      auctions.map(a => a.id),
+      async ctx => {
+        const matchedAuction = auctions.find(
+          auction => auction.id === ctx.match[0]
+        );
+        await ctx.reply(JSON.stringify(matchedAuction, null, 2));
+
+      }
+    );
+  });
+
+  adminBot.command('list_bids', async ctx => {
+    await ctx.reply('List of bids');
+    const bidsController = new BidVolunteerController(ctx);
+    await bidsController.getListOfBets();
+  });
+
+  adminBot.command('send_message_to_user', async ctx => {
+    const [, username, message] = ctx.message.text.split(' ') as [
+      string,
+      string,
+      string
+    ];
+    const clientRepository = new ClientRepository('clients', ctx.db);
+    const client = await clientRepository.findClientByUsername(
+      username.replace('@', '')
+    );
+    if (!client) {
+      await ctx.reply('No such user');
+      return;
     }
-  );
-});
+    await clientBot.telegram.sendMessage(
+      client.chatId,
+      message || 'empty message'
+    );
+  });
 
-adminBot.command('list_bids', async ctx => {
-  ctx.reply('List of bids');
-  const bidsController = new BidVolunteerController(ctx);
-  await bidsController.getListOfBets();
-});
-
-adminBot.command('send_message_to_user', async ctx => {
-  const [, username, message] = ctx.message.text.split(' ') as [
-    string,
-    string,
-    string
-  ];
-  const clientRepository = new ClientRepository('clients', ctx.db);
-  const client = await clientRepository.findClientByUsername(
-    username.replace('@', '')
-  );
-  if (!client) {
-    await ctx.reply('No such user');
-    return;
-  }
-  await clientBot.telegram.sendMessage(
-    client.chatId,
-    message || 'empty message'
-  );
-});
-
-adminBot.command('about', ctx => {
-  ctx.reply(`Користуватися ботом дуже легко. Дивися:
+  adminBot.command('about', ctx => {
+    ctx.reply(`Користуватися ботом дуже легко. Дивися:
 /create - Створити аукціон. Придумай назву для аукціону, додай фото, початкову ставку та опис (деталі, ціль збору коштів та все, щоб заохотити учасників).
 Коли аукціон створено, ти отримаєш готове посилання на бота для учасників. Поділися ним в соціальних мережах, аби більше людей знали про твій аукціон.
 /bets - Подивитися останні 3 ставки.
 `);
-});
+  });
 
-adminBot.command('show_link', ctx => {
-  const auctionId = ctx.message.text.split(' ')[1];
-  if (!auctionId) {
-    ctx.reply('please pass the id, run /show_link ID');
-    return;
-  }
-  console.log('id', auctionId);
-  ctx.reply(`https://t.me/${process.env.AUCTION_BOT_NAME}?start=${auctionId}`);
-});
+  adminBot.command('show_link', ctx => {
+    const auctionId = ctx.message.text.split(' ')[1];
+    if (!auctionId) {
+      ctx.reply('please pass the id, run /show_link ID');
+      return;
+    }
+    console.log('id', auctionId);
+    ctx.reply(
+      `https://t.me/${process.env.AUCTION_BOT_NAME}?start=${auctionId}`
+    );
+  });
 
-adminBot.command('bids', async ctx => {
-  const bidController = new BidVolunteerController(ctx);
-  await bidController.handleHighestBid();
-});
+  adminBot.command('bids', async ctx => {
+    const bidController = new BidVolunteerController(ctx);
+    await bidController.handleHighestBid();
+  });
 
-adminBot.command('close', async ctx => {
-  const bidRepository = new BidRepository(ctx.db);
-  const {activeAuction} = ctx.session;
-  if (!activeAuction) {
-    await ctx.reply(
-      'В вас наразі немає активного аукціону. Щоб створити команда /create'
-    );
-    return;
-  }
-  const highestBid = await bidRepository.findHighest(activeAuction.id);
-  const clientRepository = new ClientRepository('clients', ctx.db);
-  const client = await clientRepository.findById(highestBid.clientId);
-  if (!client) {
-    await ctx.reply(
-      'Щось не так, може бути. Мабуть, треба врчуну подивитись /bids та написати в ручну, сорі за це('
-    );
-    console.log(
-      'What do we really do in this case? Suggesting to contact devs?'
-    );
-    return;
-  }
-  try {
-    await clientBot.telegram.sendMessage(
-      client.chatId,
-      'Привіт, ти виграв на аукціоні, поставити сюди текст!'
-    );
-  } catch (err) {
-    console.error(err);
-    await ctx.reply(
-      'Щось не так, може бути. Мабуть, треба врчуну подивитись /bids та написати в ручну, сорі за це('
-    );
-    return;
-  }
-  await ctx.reply(`Вітаю, аукціон «Назва» завершено!
+  adminBot.command('close', async ctx => {
+    const bidRepository = new BidRepository(ctx.db);
+    const {activeAuction} = ctx.session;
+    if (!activeAuction) {
+      await ctx.reply(
+        'В вас наразі немає активного аукціону. Щоб створити команда /create'
+      );
+      return;
+    }
+    const highestBid = await bidRepository.findHighest(activeAuction.id);
+    const clientRepository = new ClientRepository('clients', ctx.db);
+    const client = await clientRepository.findById(highestBid.clientId);
+    if (!client) {
+      await ctx.reply(
+        'Щось не так, може бути. Мабуть, треба врчуну подивитись /bids та написати в ручну, сорі за це('
+      );
+      console.log(
+        'What do we really do in this case? Suggesting to contact devs?'
+      );
+      return;
+    }
+    try {
+      await clientBot.telegram.sendMessage(
+        client.chatId,
+        'Привіт, ти виграв на аукціоні, поставити сюди текст!'
+      );
+    } catch (err) {
+      console.error(err);
+      await ctx.reply(
+        'Щось не так, може бути. Мабуть, треба врчуну подивитись /bids та написати в ручну, сорі за це('
+      );
+      return;
+    }
+    await ctx.reply(`Вітаю, аукціон «Назва» завершено!
 Переможцем став @${client.username}.
 Переможна ставка склала UAH ${highestBid.amount}
 Бот відправив привітальне повідомлення. 
 Не забудь надіслати деталі оплати та доставки.
 
 Гайда створювати новий аукціон? ( /create )`);
+  });
+
+  launchBot(adminBot);
 });
-
-launchBot(adminBot);
-
 // Enable graceful stop
 process.once('SIGINT', () => adminBot.stop('SIGINT'));
 process.once('SIGTERM', () => adminBot.stop('SIGTERM'));
--- src/clientBot.ts
+++ src/clientBot.ts
@@ -1,14 +1,16 @@
-import {Telegraf, session} from 'telegraf';
+import {Telegraf} from 'telegraf';
 import 'dotenv/config';
 import {MongoClient} from 'mongodb';
 import {AppContext, ClientAppContext} from './types';
-import {AuctionRepository} from './db/AuctionRepository';
+import {Auction, AuctionRepository} from './db/AuctionRepository';
 import {
   BidController,
   BidVolunteerController,
 } from './controllers/BidController';
 import {launchBot} from './launchBot';
 import {ClientRepository} from './db/Client';
+import {getDb} from './db/connection';
+import {session} from 'telegraf-session-mongodb';
 
 const DB_URL = process.env.DB_URL;
 const DB_NAME = process.env.DB_NAME;
@@ -23,106 +25,116 @@ if (!DB_URL || !DB_NAME) {
 
 export const clientBot = new Telegraf<ClientAppContext>(process.env.BOT_TOKEN);
 
-// TODO: this is a deprecated in-memory session, we might want to use a different one
-clientBot.use(session());
-clientBot.use(async (ctx, next) => {
-  const connection = await MongoClient.connect(DB_URL);
-  ctx.db = connection.db(DB_NAME);
-  ctx.session ??= {};
-  return next();
-});
-
-clientBot.start(async ctx => {
-  const auctionId = ctx.startPayload;
-  const auctionRepo = new AuctionRepository(ctx.db);
-  const auction = await auctionRepo.findOne(auctionId);
-  if (!auction) {
-    ctx.reply('Щось не так, нема такого аукціону(');
-    ctx.reply(JSON.stringify(await auctionRepo.findAll(), null, 2));
-    return;
-  }
-  ctx.session.auction = auction;
-  const clientRepository = new ClientRepository('clients', ctx.db);
-  ctx.session.client = await clientRepository.register({
-    telegramId: ctx.from.id,
-    username: ctx.from.username,
-    chatId: ctx.chat.id,
+getDb().then(db => {
+  clientBot.use(session(db, {collectionName: 'clientSessions'}));
+  clientBot.use(async (ctx, next) => {
+    ctx.db = db;
+    ctx.session ??= {};
+    return next();
   });
-  // console.log('auction.photos', auction.photos);
 
-  const caption = `${auction.title}
-${auction.description}`;
-  ctx.reply(caption);
-  const photoSizes = auction.photos;
-  console.log(
-    'photo sizes',
-    photoSizes,
-    photoSizes[photoSizes.length - 1].file_id
-  );
-  try {
-    await ctx.replyWithPhoto(photoSizes[photoSizes.length - 1].file_id, {
-      caption,
+  clientBot.start(async ctx => {
+    const auctionId = ctx.startPayload || null;
+    if (!auctionId) {
+      await ctx.reply(
+        'Наразі ми не підтримуємо пошук аукціиону, ви можете перейти за лінкою конкретного аукціону, яку кинув волонтер для того щоб робити ставки'
+      );
+      return;
+    }
+    const auctionRepo = new AuctionRepository(ctx.db);
+    let auction: Auction | null = null;
+    try {
+      auction = await auctionRepo.findOne(auctionId);
+    } catch (err) {
+      await ctx.reply('Щось не так, нема такого аукціону(');
+      return;
+    }
+    if (!auction) {
+      await ctx.reply('Щось не так, нема такого аукціону(');
+      return;
+    }
+    ctx.session.auction = auction;
+    const clientRepository = new ClientRepository('clients', ctx.db);
+    ctx.session.client = await clientRepository.register({
+      telegramId: ctx.from.id,
+      username: ctx.from.username,
+      chatId: ctx.chat.id,
     });
-  } catch (err) {
-    await ctx.reply(caption);
-  }
+    // console.log('auction.photos', auction.photos);
+
+    const caption = `${auction.title}
+${auction.description}`;
+    ctx.reply(caption);
+    const photoSizes = auction.photos;
+    console.log(
+      'photo sizes',
+      photoSizes,
+      photoSizes[photoSizes.length - 1].file_id
+    );
+    try {
+      await ctx.replyWithPhoto(photoSizes[photoSizes.length - 1].file_id, {
+        caption,
+      });
+    } catch (err) {
+      await ctx.reply(caption);
+    }
+  });
+
+  clientBot.command('test', async ctx => {
+    ctx.reply('Hello!');
+    ctx.reply("I'm 'hackaton-auction-bot'");
+    ctx.reply(
+      `You are @${ctx.message.from.username}. Your id – ${ctx.message.from.id}`
+    );
+    await ctx.replyWithPhoto(
+      'AgACAgIAAxkBAAMfYqslt567Db5L6qnn0a7RjFM9OnYAAlq8MRvH22FJgbWozqFukfgBAAMCAANzAAMkBA'
+    );
+    await ctx.reply(
+      (
+        await clientBot.telegram.getFileLink(
+          'AgACAgIAAxkBAAIBD2Ksxa19fZ77rspkVeA_YscVKD4sAAI-vDEbUpphSU35cEKY_DSeAQADAgADcwADJAQ'
+        )
+      ).href
+    );
+    await ctx.replyWithPhoto(
+      'AgACAgIAAxkBAAIBD2Ksxa19fZ77rspkVeA_YscVKD4sAAI-vDEbUpphSU35cEKY_DSeAQADAgADcwADJAQ'
+    );
+    // clientBot.on('photo', ctx => {
+    //   ctx.reply(JSON.stringify(ctx.message.photo, null, 2));
+    //   ctx.replyWithPhoto(ctx.message.photo[0].file_id);
+    // });
+  });
+
+  clientBot.command('make_bid', async ctx => {
+    const betController = new BidController(ctx);
+
+    await betController.makeBid();
+  });
+
+  clientBot.command('subscribe', ctx => {
+    ctx.reply('Ви підписались на оновлення');
+  });
+
+  clientBot.command('unsubscribe', ctx => {
+    ctx.reply('Ви відписались від оновлень');
+  });
+
+  clientBot.command('max_bid', ctx => {
+    ctx.reply('Максимальна ставка 100500');
+  });
+
+  clientBot.command('about', ctx => {
+    ctx.reply('У цьому боті ви можете робити ставку');
+  });
+
+  clientBot.command('all', async ctx => {
+    const tmp = new BidVolunteerController(ctx as any);
+
+    await tmp.getListOfBets();
+  });
+
+  launchBot(clientBot);
 });
-
-clientBot.command('test', async ctx => {
-  ctx.reply('Hello!');
-  ctx.reply("I'm 'hackaton-auction-bot'");
-  ctx.reply(
-    `You are @${ctx.message.from.username}. Your id – ${ctx.message.from.id}`
-  );
-  await ctx.replyWithPhoto(
-    'AgACAgIAAxkBAAMfYqslt567Db5L6qnn0a7RjFM9OnYAAlq8MRvH22FJgbWozqFukfgBAAMCAANzAAMkBA'
-  );
-  await ctx.reply(
-    (
-      await clientBot.telegram.getFileLink(
-        'AgACAgIAAxkBAAIBD2Ksxa19fZ77rspkVeA_YscVKD4sAAI-vDEbUpphSU35cEKY_DSeAQADAgADcwADJAQ'
-      )
-    ).href
-  );
-  await ctx.replyWithPhoto(
-    'AgACAgIAAxkBAAIBD2Ksxa19fZ77rspkVeA_YscVKD4sAAI-vDEbUpphSU35cEKY_DSeAQADAgADcwADJAQ'
-  );
-  // clientBot.on('photo', ctx => {
-  //   ctx.reply(JSON.stringify(ctx.message.photo, null, 2));
-  //   ctx.replyWithPhoto(ctx.message.photo[0].file_id);
-  // });
-});
-
-clientBot.command('make_bid', async ctx => {
-  const betController = new BidController(ctx);
-
-  await betController.makeBid();
-});
-
-clientBot.command('subscribe', ctx => {
-  ctx.reply('Ви підписались на оновлення');
-});
-
-clientBot.command('unsubscribe', ctx => {
-  ctx.reply('Ви відписались від оновлень');
-});
-
-clientBot.command('max_bid', ctx => {
-  ctx.reply('Максимальна ставка 100500');
-});
-
-clientBot.command('about', ctx => {
-  ctx.reply('У цьому боті ви можете робити ставку');
-});
-
-clientBot.command('all', async ctx => {
-  const tmp = new BidVolunteerController(ctx as any);
-
-  await tmp.getListOfBets();
-});
-
-launchBot(clientBot);
-
 // Enable graceful stop
 process.once('SIGINT', () => clientBot.stop('SIGINT'));
 process.once('SIGTERM', () => clientBot.stop('SIGTERM'));
